3be9dc821b0632d321e83d9a15c76808510e18b3
Paolo Mottadelli <paolo.moz@gmail.com>
2023-06-24 07:12:02 +0200

    nav expand with transition

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -57,6 +57,7 @@ function toggleMenu(nav, navSections, forceExpanded = null) {
   const button = nav.querySelector('.nav-hamburger button');
   document.body.style.overflowY = (expanded || isDesktop.matches) ? '' : 'hidden';
   nav.setAttribute('aria-expanded', expanded ? 'false' : 'true');
+  document.querySelector('header').setAttribute('aria-expanded', expanded ? 'false' : 'true');
   toggleAllNavSections(navSections, expanded || isDesktop.matches ? 'false' : 'true');
   button.setAttribute('aria-label', expanded ? 'Open navigation' : 'Close navigation');
   // enable nav dropdown keyboard accessibility
